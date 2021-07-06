8b3d8c6acedf54d2aa580d96ad3a3fe8f958b700
marcel <marcel_feige@hotmail.de>
2021-07-06 16:52:42 +0200

    Gebrauchtwagenpreis bei 50k

--- LinRegressionGebrauchtwagen.py
+++ LinRegressionGebrauchtwagen.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
 df = pd.read_csv(
-    "D:\Eigene Datein\eLearning Kurse\Machine Learning"
-    "\Kursmaterialien\Abschnitt 05 - Lineare Regression\wohnungspreise.csv")
+    "D:\Eigene Datein\\eLearning Kurse\\Machine Learning\Kursmaterialien\\"
+    "Abschnitt 06 - Projekt Lineare Regression\\autos_prepared.csv")
 
 
-plt.scatter(df["Quadratmeter"], df["Verkaufspreis"])
+plt.scatter(df["kilometer"], df["price"])
 #plt.show()
 
 model = LinearRegression()
-model.fit(df[["Quadratmeter"]], df[["Verkaufspreis"]])
+model.fit(df[["kilometer"]], df[["price"]])
 
 # Verkaufspreis = Intercept + Coef * x
 print("Intercept: " + str(model.intercept_))
@@ -19,10 +19,12 @@ print("Coef: " + str(model.coef_))
 
 # Vorhersagen durch das Model
 
-min_x = min(df["Quadratmeter"])
-max_x = max(df["Quadratmeter"])
+min_x = min(df["kilometer"])
+max_x = max(df["kilometer"])
+
+p50k = model.predict([[50000]])
+print("Preis bei 50.000 km: " + str(p50k))
 
 predicted = model.predict([[min_x], [max_x]])
 plt.plot([min_x, max_x], predicted, color = "red")
 plt.show()
-
