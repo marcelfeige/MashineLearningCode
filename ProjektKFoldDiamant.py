c83f0a28625c8962cc6cdb4757be413331e1916f
marcel <marcel_feige@hotmail.de>
2021-07-07 15:30:08 +0200

    initial commit

---
+++ ProjektKFoldDiamant.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import KFold
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedKFold
+
+df = pd.read_csv(
+    "D:\Eigene Datein\eLearning Kurse\Machine Learning\Kursmaterialien\Abschnitt 13 - Praxis Polynomiale Regression\diamonds.csv")
+
+print(df.head())
+
+X_carat = df[["carat"]].values
+X_LBH = df[["x", "y", "z"]].values
+Y = df[["price"]]
+
+# Lin Regression auf Basis Carat
+scores_carat = cross_val_score(LinearRegression(), X_carat, Y, cv = RepeatedKFold(n_repeats = 10))
+# print("Scores Carat : " + str(scores_carat))
+mean_carat = np.mean(scores_carat)
+print("Mean Scores Carat: " + str(mean_carat))
+
+# Lin Regression auf Basis Laenge, Breite, Hoehe
+scores_LBH = cross_val_score(LinearRegression(), X_LBH, Y, cv = RepeatedKFold(n_repeats = 1000))
+# print("Scores Carat : " + str(scores_LBH))
+mean_LBH = np.mean(scores_LBH)
+print("Mean Scores LBH: " + str(mean_LBH))
