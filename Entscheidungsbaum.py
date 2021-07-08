7bfc711fe14b2e16a9f97d90aceb2bebcde6df85
marcel <marcel_feige@hotmail.de>
2021-07-08 15:53:18 +0200

    beschraenkungen

--- Entscheidungsbaum.py
+++ Entscheidungsbaum.py
@@ -29,13 +29,13 @@ y = df["success"].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0, test_size = 0.25)
 
-model = DecisionTreeClassifier(criterion = "entropy")
+model = DecisionTreeClassifier(criterion = "entropy", max_depth = 4, min_samples_leaf = 3)
 model.fit(X_train, y_train)
 
 print(model.score(X_test, y_test))
 
 # Trainings-Daten plotten
-#plot_classifier(model, X_train, y_train, proba = False, xlabel = "Alter", ylabel = "Interesse")
+plot_classifier(model, X_train, y_train, proba = False, xlabel = "Alter", ylabel = "Interesse")
 
 plt.figure(dpi = 200)
 
